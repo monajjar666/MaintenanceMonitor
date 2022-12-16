6e1f2344c38683f9030212d7b6fdbb7ba4804c8c
Mo <ic21b001@technikum-wien.at>
2022-12-16 17:30:42 +0100

    initial commit

---
+++ src/main/java/at/fhtw/bic/monitor/MaintenanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/bic/monitor/MaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
